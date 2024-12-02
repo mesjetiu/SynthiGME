04dc7b53f8c4c42cfc4d16e5f0bc4a964d099a60
Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
2024-12-02 12:57:58 +0100

    añadida licencia a archivo py

--- standalone/Windows/synthigme_windows.py
+++ standalone/Windows/synthigme_windows.py
@@ -1,3 +1,23 @@
+"""
+This file is part of SynthiGME.
+
+SynthiGME is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SynthiGME is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SynthiGME.  If not, see <https://www.gnu.org/licenses/>.
+
+Copyright 2024 Carlos Arturo Guerra Parra <carlosarturoguerra@gmail.com>
+"""
+
+
 import subprocess
 import threading
 import os
